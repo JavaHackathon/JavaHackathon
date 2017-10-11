28abd0b2e850bacb7f49e0a7857771a790afe9ad
Joshua May <jmay89@icloud.com>
2017-10-11 16:18:26 -0400

    basic structure based on api call project

--- src/main/java/com/example/hackathon/Locations.java
+++ src/main/java/com/example/hackathon/Locations.java
@@ -1,4 +1,29 @@
 package com.example.hackathon;
 
-public class Locations {
+import java.util.List;
+import java.util.ArrayList;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.web.client.RestTemplate;
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public class Locations {
+        static final String API_URL = "https://maps.googleapis.com/maps/api/place/nearbysearch/output?parametersAIzaSyCwjWWNj0dGJsoYs91DvxGRXOnDahowTUg";
+
+
+        private String title;
+        @JsonProperty(value = "poster_path")
+
+        private String name
+        private String overView;
+        private double popularity;
+
+
+        //
+        // Beans Below
+        //
+
+        public Movie() {
+
+        }
 }
--- src/main/java/com/example/hackathon/LocationsController.java
+++ src/main/java/com/example/hackathon/LocationsController.java
@@ -1,5 +1,49 @@
 package com.example.hackathon;
 
 public interface LocationsController {
-    
+    import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.ui.Model;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+//
+//
+//
+
+    @Controller
+    public class LocationsController {
+        static final String API_URL = "https://maps.googleapis.com/maps/api/place/nearbysearch/output?parametersAIzaSyCwjWWNj0dGJsoYs91DvxGRXOnDahowTUg";
+//
+// api key is as follows: AIzaSyCwjWWNj0dGJsoYs91DvxGRXOnDahowTUg
+//
+
+        @RequestMapping(path = "/", method = RequestMethod.GET)
+        public String home() {
+            return "home"; //returns homepage path
+        }
+
+
+        // link up medium-popular-long-name.html
+        @RequestMapping(path = "/Results", method = RequestMethod.GET)
+        public String mediumPop(Model model) {
+            List<Place> movies = getPlaces("/medium-popular-long-name").stream()
+
+            model.addAttribute("places", places);
+            //above dictates the template
+            return "Results";
+        }
+
+
+        public static List<Place> getPlaces(String route) {
+            RestTemplate restTemplate = new RestTemplate();
+            ResultsPage results = restTemplate.getForObject(API_URL, ResultsPage.class);
+            return results.getResults();
+
+
+        }}
+
 }
