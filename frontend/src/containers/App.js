101a6ca34e73af0f84531f286305113bc80280b7
Chad Yates <cayates1989@gmail.com>
2017-10-11 15:56:23 -0400

    adjusting the src/index.js file to have browser router and links

--- frontend/src/containers/App.js
+++ frontend/src/containers/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
 
+import SearchForm from '../components/SearchForm';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+        <h1>Restaurant Locator (For Now)</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <SearchForm />
+      </div>              
     );
   }
 }
