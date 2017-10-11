f2ddb3d56fa45799205069ca550371f5c55e348f
Ryan Kelley <rtkelley@gmail.com>
2017-10-11 16:22:52 -0400

    added form to form page

--- frontend/src/components/SearchForm.js
+++ frontend/src/components/SearchForm.js
@@ -1,19 +1,38 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
-import {connect} from 'react-redux';
+import {connect} from react-redux;
 import {getResults} from '../actions/';
 import { bindActionCreators } from 'redux';
 import { Link } from 'react-router-dom';
-import { checkbox, textfield, typography, button, form-field } from 'material-components-web';
+import { checkbox, textfield, button, form-field } from 'material-components-web';
 
 class SearchForm extends Component {
   render() {
     return (
-      <div className="search">
-        <form>
-
-        </form>
-      </div>
+      <form action="POST" className="search">
+        <h1 className="mdc-typography--display2">Search for restaurants near you</h1>
+        <div className="mdc-form-field">
+          <div className="mdc-textfield">
+            <label for="location" class="mdc-textfield__label">
+            Location
+            </label>
+            <input id="location" type="text" class="mdc-textfield__input" placeholder="e.g. New York"/>
+          </div>
+          <div className="mdc-textfield">
+            <label for="type" class="mdc-textfield__label">
+            Type of Restaurant
+            </label>
+            <input id="type" type="text" class="mdc-textfield__input" placeholder="e.g. Polish"/>
+          </div>
+          <button type="submit"
+            class="mdc-button
+                   mdc-button--raised
+                   mdc-ripple-surface"
+            data-mdc-auto-init="MDCRipple">
+              Search!
+          </button>
+        </div>
+      </form>
     );
   }
 }
