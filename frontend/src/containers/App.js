bddf67ae9068cebdd7b6506d73397efbf2699bf5
Chad Yates <cayates1989@gmail.com>
2017-10-12 14:22:58 -0400

    added some styling while we are working on connecting to local host 8080

--- frontend/src/containers/App.js
+++ frontend/src/containers/App.js
@@ -7,13 +7,12 @@ import SearchForm from '../components/SearchForm';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-        <h1>Restaurant Locator (For Now)</h1>
-        </header>
-        <SearchForm 
-          
-        />
+      <div className="landing-page">
+      <div className="jumbotron text-center make-transparent">
+        <h1 className="display-3">Troll your City!</h1>
+        <p className="lead">Find out about places in your area!</p>
+        <SearchForm/>
+      </div>
       </div>
     );
   }
