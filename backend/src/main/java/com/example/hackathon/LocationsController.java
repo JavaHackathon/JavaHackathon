6691f41a8ee6ca8e3460301d4e3f2c096ccb9b63
Joshua May <jmay89@icloud.com>
2017-10-14 18:06:04 -0400

    fixed, hit the route

--- backend/src/main/java/com/example/hackathon/LocationsController.java
+++ backend/src/main/java/com/example/hackathon/LocationsController.java
@@ -1,71 +1,52 @@
 package com.example.hackathon;
 
-
-import org.springframework.http.ResponseEntity;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.ui.Model;
+import se.walkercrou.places.GooglePlaces;
+import se.walkercrou.places.Param;
+import se.walkercrou.places.Place;
+import se.walkercrou.places.exception.GooglePlacesException;
 
-import javax.xml.stream.Location;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
 public class LocationsController {
 
+    private static GooglePlaces client = new GooglePlaces("AIzaSyCwjWWNj0dGJsoYs91DvxGRXOnDahowTUg");
 
+        // make the ajax call to localhost:8080/google-places/ for front end
+    @RequestMapping(path = "/google-places/", method = RequestMethod.GET)
+    public List<JsonNode> getGoogleSearchResult(@RequestParam("searchterm") final String searchQuery) throws Exception {
+        // whatever $searchterm is = will be the searchQuery (spring puts this here)
+        // workaround because could not get jackson to parse json. jackson does not understand "place" but understands json node
+        // Example of adding extra Parameters to the Places API request https://developers.google.com/places/web-service/search
+        // final Param typeParam = new Param("type");
+        // typeParam.value("restaurant);
+        // placeQueryResults = client.getPlacesByQuery(searchQuery, 10, typeParam);
 
-   // public class LocationsController {
-        public static String keyword = "";
-        static final String API_URL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?" + keyword + "AIzaSyCwjWWNj0dGJsoYs91DvxGRXOnDahowTUg";
-        private String searchTerm;
+        List<Place> placeQueryResults = new ArrayList<>();
 
-    //
-// api key is as follows: AIzaSyCwjWWNj0dGJsoYs91DvxGRXOnDahowTUg
-//
-    public String buildUrl(String keyword) {
-        return "https://maps.googleapis.com/maps/api/place/nearbysearch/json?" + keyword + "AIzaSyCwjWWNj0dGJsoYs91DvxGRXOnDahowTUg";
+        try {
+            placeQueryResults = client.getPlacesByQuery(searchQuery, 10);
+        } catch (GooglePlacesException e) {
+            System.out.println("no results found;");
+        }
+
+        final List <JsonNode> result = new ArrayList<>();
+        final ObjectMapper mapper = new ObjectMapper();
+
+        for (final Place place : placeQueryResults) {
+            result.add(mapper.readTree(place.getJson().toString()));
+            // we convert place into json object and then convert into a string and
+            // we read with objMapper to map to json node which it reads
+        }
+
+        return result;
     }
 
-    //
-    //below is pseudocode but not complete
-    //
-
-    @RequestMapping(path = "/get-location/", method = RequestMethod.GET)
-    public String getLocation(@RequestBody Locations location) {
-        String searchTerm = location.getApiUrl();
-       String newUrl = buildUrl(searchTerm);
-        return newUrl;
-        
-    }
-
-    //
-    // normal paths below
-    //
-
-        @RequestMapping(path = "/", method = RequestMethod.GET)
-        public String home() {
-            return "home"; //returns homepage path
-        }
-
-
-        // link up Results page
-        @RequestMapping(path = "/Results", method = RequestMethod.GET)
-        public List<Locations> getPlaces() { // do i need a stream?
-
-            return getPlaces();
-        }
-
-
-        public static List<Locations> getPlaces(String route) {
-            RestTemplate restTemplate = new RestTemplate();
-            ResultsPage results = restTemplate.getForObject(API_URL, ResultsPage.class);
-            return results.getResults();
-
-
-        }
 }
 
 
